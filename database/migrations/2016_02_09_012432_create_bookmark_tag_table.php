f84bb5ec4bb7d18d94916a714767c0835cadf32c
Ethan Luke Stenis <ethanstenis@gmail.com>
2016-02-08 19:35:37 -0600

    Initial commit

---
+++ database/migrations/2016_02_09_012432_create_bookmark_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookmarkTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmark_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('bookmark_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bookmark_tag');
+    }
+}
